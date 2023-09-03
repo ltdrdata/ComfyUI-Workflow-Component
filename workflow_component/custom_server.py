e68508e27b4872ee615ddea24e7f1601c6617cf5
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-09-03 12:49:17 +0900

    refactor: use onprompt handler instead of endpoint hooking
    based on:
    https://github.com/comfyanonymous/ComfyUI/pull/765

--- workflow_component/custom_server.py
+++ workflow_component/custom_server.py
@@ -1,58 +1,12 @@
 import os
-import uuid
 import server
 from aiohttp import web
 
 import workflow_component.component_loader as component_loader
 import workflow_component.workflow_execution as workflow_execution
-import workflow_component.execution_experimental as execution_experimental
 
 
-
-async def original_post_prompt(request):
-    self = server.PromptServer.instance
-
-    print("got prompt")
-    resp_code = 200
-    out_string = ""
-    json_data = await request.json()
-
-    if "number" in json_data:
-        number = float(json_data['number'])
-    else:
-        number = self.number
-        if "front" in json_data:
-            if json_data['front']:
-                number = -number
-
-        self.number += 1
-
-    if "prompt" in json_data:
-        prompt = json_data["prompt"]
-        valid = execution_experimental.validate_prompt(prompt)
-        extra_data = {}
-        if "extra_data" in json_data:
-            extra_data = json_data["extra_data"]
-
-        if "client_id" in json_data:
-            extra_data["client_id"] = json_data["client_id"]
-        if valid[0]:
-            prompt_id = str(uuid.uuid4())
-            outputs_to_execute = valid[2]
-            self.prompt_queue.put((number, prompt_id, prompt, extra_data, outputs_to_execute))
-            response = {"prompt_id": prompt_id, "number": number, "node_errors": valid[3]}
-            return web.json_response(response)
-        else:
-            print("invalid prompt:", valid[1])
-            return web.json_response({"error": valid[1], "node_errors": valid[3]}, status=400)
-    else:
-        return web.json_response({"error": "no prompt", "node_errors": []}, status=400)
-
-
-@server.PromptServer.instance.routes.post("/prompt")
-async def prompt_hook(request):
-    json_data = await request.json()
-
+def onprompt(json_data):
     extra_data = json_data.get('extra_data', {})
     extra_pnginfo = extra_data.get('extra_pnginfo', {})
     workflow = extra_pnginfo.get('workflow', {})
@@ -82,10 +36,10 @@ async def prompt_hook(request):
         prompt = json_data["prompt"]
         workflow_execution.garbage_collect(prompt.keys())
 
-    return await original_post_prompt(request)
+    return json_data
 
 
-server.PromptServer.instance.app.router.add_post('/prompt', prompt_hook)
+server.PromptServer.instance.add_on_prompt_handler(onprompt)
 
 
 import json
